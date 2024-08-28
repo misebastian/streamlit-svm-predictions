58c4d786a1873f1dccfe25afcc0a68dfcbd774d0
misebastian <152098343+misebastian@users.noreply.github.com>
2024-08-28 00:08:16 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -9,24 +9,26 @@ model = joblib.load('best_logistic_model.pkl')
 st.title("Predicción con Regresión Logística")
 
 # Crear los inputs que coinciden con las características del modelo
-Temp_C = st.number_input('Temp_C')
-Dew_Point_Temp_C = st.number_input('Dew Point Temp_C')
-Rel_Hum = st.number_input('Rel Hum %')
-Wind_Speed_km_h = st.number_input('Wind Speed km/h')
-Visibility_km = st.number_input('Visibility km')
-Press_kPa = st.number_input('Press kPa')
-Month = st.number_input('Month')
-Day = st.number_input('Day')
-Hour = st.number_input('Hour')
+Temp_C = st.number_input('Temp_C', value=0.0, format="%.2f")
+Dew_Point_Temp_C = st.number_input('Dew Point Temp_C', value=0.0, format="%.2f")
+Rel_Hum = st.number_input('Rel Hum %', value=0)
+Wind_Speed_km_h = st.number_input('Wind Speed km/h', value=0)
+Visibility_km = st.number_input('Visibility km', value=0.0, format="%.2f")
+Press_kPa = st.number_input('Press kPa', value=0.0, format="%.2f")
+Month = st.number_input('Month', value=1, min_value=1, max_value=12)
+Day = st.number_input('Day', value=1, min_value=1, max_value=31)
+Hour = st.number_input('Hour', value=0, min_value=0, max_value=23)
 
 # Crear un botón para realizar la predicción
 if st.button('Realizar Predicción'):
-    # Crear un DataFrame con los valores introducidos
+    # Convertir los valores a los tipos correctos si es necesario
     input_data = pd.DataFrame([[Temp_C, Dew_Point_Temp_C, Rel_Hum, Wind_Speed_km_h, Visibility_km, Press_kPa, Month, Day, Hour]],
                               columns=['Temp_C', 'Dew Point Temp_C', 'Rel Hum %', 'Wind Speed_km/h', 'Visibility_km', 'Press kPa', 'Month', 'Day', 'Hour'])
-    
+
     # Realizar la predicción
-    prediction = model.predict(input_data)
-    
-    # Mostrar el resultado
-    st.write(f"La predicción es: {prediction[0]}")
+    try:
+        prediction = model.predict(input_data)
+        st.write(f"La predicción es: {prediction[0]}")
+    except Exception as e:
+        st.write(f"Error al realizar la predicción: {e}")
+
