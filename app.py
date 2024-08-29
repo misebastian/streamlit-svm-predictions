cba2d48eab1ccc2b3279e9df0f73870ecf3295d9
misebastian <152098343+misebastian@users.noreply.github.com>
2024-08-28 21:25:30 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -3,6 +3,17 @@ import streamlit as st
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 
+# Datos predefinidos de media y desviación estándar
+data = {
+    'Temp_C': [8.798144, 11.687883],
+    'Dew Point Temp_C': [2.555294, 10.883072],
+    'Rel Hum_%': [67.431694, 16.918881],
+    'Wind Speed_km/h': [14.945469, 8.688696],
+    'Visibility_km': [27.664447, 12.622688],
+    'Press_kPa': [101.051623, 0.844005]
+}
+df = pd.DataFrame(data, index=['mean', 'std'])
+
 # Cargar el modelo guardado con joblib
 model = joblib.load('best_logistic_model.pkl')
 
@@ -14,28 +25,31 @@ st.write("Por favor, introduce los valores a continuación:")
 col1, col2 = st.columns(2)
 
 with col1:
-    Temp_C = st.number_input('Temp_C', value=0.0, format="%.2f")
-    Dew_Point_Temp_C = st.number_input('Dew Point Temp_C', value=0.0, format="%.2f")
-    Rel_Hum = st.number_input('Rel Hum_%', value=0)
-    Wind_Speed_km_h = st.number_input('Wind Speed_km/h', value=0)
-    Visibility_km = st.number_input('Visibility_km', value=0.0, format="%.2f")
+    Temp_C = st.number_input('Temp_C', format="%.2f")
+    Dew_Point_Temp_C = st.number_input('Dew Point Temp_C', format="%.2f")
+    Rel_Hum = st.number_input('Rel Hum_%')
+    Wind_Speed_km_h = st.number_input('Wind Speed_km/h')
+    Visibility_km = st.number_input('Visibility_km', format="%.2f")
 
 with col2:
-    Press_kPa = st.number_input('Press_kPa', value=0.0, format="%.2f")
+    Press_kPa = st.number_input('Press_kPa', format="%.2f")
     Month = st.number_input('Month', value=1, min_value=1, max_value=12)
     Day = st.number_input('Day', value=1, min_value=1, max_value=31)
     Hour = st.number_input('Hour', value=0, min_value=0, max_value=23)
 
 # Crear un botón para realizar la predicción
 if st.button('Realizar Predicción'):
-    # Asegurarse de que los nombres de las columnas coincidan exactamente con los usados durante el entrenamiento
+    # Definir los inputs en un DataFrame
     input_data = pd.DataFrame([[Temp_C, Dew_Point_Temp_C, Rel_Hum, Wind_Speed_km_h, Visibility_km, Press_kPa, Month, Day, Hour]],
                               columns=['Temp_C', 'Dew Point Temp_C', 'Rel Hum_%', 'Wind Speed_km/h', 'Visibility_km', 'Press_kPa', 'Month', 'Day', 'Hour'])
 
-    # Estandarizar las columnas numéricas
-    scaler = StandardScaler()
-    columns_to_scale = ["Temp_C", "Dew Point Temp_C", "Rel Hum_%", "Wind Speed_km/h", "Visibility_km", "Press_kPa"]
-    input_data[columns_to_scale] = scaler.fit_transform(input_data[columns_to_scale])
+    # Reemplazar valores faltantes con las medias predefinidas
+    for column in ['Temp_C', 'Dew Point Temp_C', 'Rel Hum_%', 'Wind Speed_km/h', 'Visibility_km', 'Press_kPa']:
+        if pd.isna(input_data[column][0]):
+            input_data[column] = df.at['mean', column]
+    
+    # Estandarizar las columnas numéricas en una sola línea
+    input_data[["Temp_C", "Dew Point Temp_C", "Rel Hum_%", "Wind Speed_km/h", "Visibility_km", "Press_kPa"]] = (input_data[["Temp_C", "Dew Point Temp_C", "Rel Hum_%", "Wind Speed_km/h", "Visibility_km", "Press_kPa"]] - df.loc['mean']) / df.loc['std']
 
     # Realizar la predicción
     prediction = model.predict(input_data)
